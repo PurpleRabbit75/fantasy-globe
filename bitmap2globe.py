ad9785b75f23d6bd3892608a80995e4ceec078a2
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-07-27 02:13:04 +0300

    Chopped unneeded dependencies

--- bitmap2globe.py
+++ bitmap2globe.py
@@ -1,12 +1,5 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 
 
 URL = "https://web.archive.org/web/20180703195015id_/https://www.maptoglobe.com/#"
@@ -52,3 +45,5 @@ def main():
     countDown(10, "Program will terminate in")
     driver.close()
     print("program has terminated")
+
+main()
